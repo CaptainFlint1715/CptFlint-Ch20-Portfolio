eeaee9c7dcca9928cb277d5fa95751a0d8c233f6
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-07-31 14:37:21 -0500

    added picture and about me text, gitignore

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import profpic from '../assets/images/prof.png'
 
 function About() {
     return (
-        <div>
-            <h1>About Me</h1>
-            <p>I like lemons.</p>
+        <div className="flex items-center">
+            <img src={profpic} alt="Profile Picture" className="mr-4" />
+            <div className="ml-14">
+                <h1 className="text-3xl font-bold text-purple mb-4">About Me</h1>
+                <p className="text-lg">Hello! I am a young professional possessing a diverse skillset and aspiring to grow my career as a programmer. Having recently graduated from the University of Minnesota's rigorous Programming Bootcamp, I have under my belt a robust portfolio of projects demonstrating my skills. Please utilize the navigation bar at the top of the page on the way to giving my work a look. Thank you and I hope to be in contact soon!</p>
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className='bg-red text-xl text-white flex items-center justify-between px-4 py-2'>
             <a href='https://github.com/CaptainFlint1715' className="cursor-pointer hover:text-gold transition">Github    </a>
             <p>    </p>
-            <a href='https://www.linkedin.com/feed/' className="cursor-pointer hover:text-gold transition">     Portfolio</a>
+            <a href='https://www.linkedin.com/feed/' className="cursor-pointer hover:text-gold transition">     LinkedIn</a>
         </div>
     )
 

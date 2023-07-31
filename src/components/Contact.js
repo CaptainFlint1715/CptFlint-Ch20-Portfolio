9fe2e7ab29b0353d4bdf45c33cd4e94e7cf9d134
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-07-31 12:58:40 -0500

    finished contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { validateEmail } from '../utils/helpers'
 
 function Contact() {
     // Create state variables for the fields in the form
     // We are also setting their initial values to an empty string
     const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
+    const [nameOf, setName] = useState('');
     const [message, setMessage] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [nameError, setNameError] = useState('');
+    const [errorMessage, setError] = useState('');
 
     const handleInputChange = (e) => {
         // Getting the value and name of the input which triggered the change
@@ -19,63 +18,96 @@ function Contact() {
         // Based on the input type, we set the state of either email, username, and password
         if (inputType === 'email') {
             setEmail(inputValue);
+            // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+            if (!validateEmail(inputValue)) {
+                setError('Email is invalid');
+                // We want to exit out of this code block if something is wrong so that the user can correct it
+                return;
+            } else {
+                setError('')
+            }
         } else if (inputType === 'message') {
             setMessage(inputValue);
-        } else if (inputType === 'name') {
+        } else if (inputType === 'nameof') {
             setName(inputValue)
         }
     };
+
+    const handleBlur = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputValue === '' && inputType === 'email') {
+            setError('Email is required');
+        } else if (inputValue === '' && inputType === 'nameof') {
+            setError('Name is required');
+        } else if (inputValue === '' && inputType === 'message') {
+            setError('Message is required');
+        } else {
+            setError('');
+        }
+    }
+
+    const isFormValid = !errorMessage
 
     const handleFormSubmit = (e) => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         e.preventDefault();
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) || !name) {
-            setErrorMessage('Email is invalid');
-            // We want to exit out of this code block if something is wrong so that the user can correct it
-            return;
-            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+        if (isFormValid) {
+            setMessage('');
+            setName('');
+            setEmail('');
+        } else {
+            setError('Please fill in all required fields and correct any errors.');
         }
-        alert(`Hello ${userName}`);
-
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
-        setMessage('');
-        setName('');
-        setEmail('');
     };
 
     return (
-        <div>
-            <form className="form">
-                <input
-                    value={email}
-                    name="email"
-                    onChange={handleInputChange}
-                    type="email"
-                    placeholder="email"
-                />
-                <input
-                    value={name}
-                    name="name"
-                    onChange={handleInputChange}
-                    type="text"
-                    placeholder="name"
-                />
-                <input
-                    value={message}
-                    name="message"
-                    onChange={handleInputChange}
-                    type="text"
-                    placeholder="message"
-                />
-                <button type="button" onClick={handleFormSubmit}>Submit</button>
-            </form>
-            {errorMessage && (
+        <div className="flex justify-center items-center h-screen">
+            <form className="form bg-gold shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                        type="email"
+                        placeholder="email"
+                        className="text-black"
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        value={nameOf}
+                        name="nameof"
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                        type="text"
+                        placeholder="name"
+                        className="text-black"
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        value={message}
+                        name="message"
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                        type="text"
+                        placeholder="message"
+                        className="text-black"
+                    />
+                </div>
+                <button className="text-black mb-6" type="button" onClick={handleFormSubmit} disabled={!isFormValid}>Submit</button>
+                {errorMessage && (
                 <div>
-                    <p className="error-text">{errorMessage}</p>
+                    <p className="error-text text-black">{errorMessage}</p>
                 </div>
             )}
+            </form>
+            
         </div>
     );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,8 +3,17 @@ import React from 'react';
 function Resume() {
     return (
         <div>
-            <h1>Resume</h1>
-            <p>I like limes.</p>
+            <h1>Skills</h1>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>NoSQL</li>
+                <li>React</li>
+            </ul>
         </div>
     )
 }

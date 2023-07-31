6f4e68dd59a80d488fb1d1fe36d5efe678abc682
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-07-31 13:25:17 -0500

    added resume section with download and skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Resume() {
     return (
-        <div>
-            <h1>Skills</h1>
-            <ul>
+        <div className="flex flex-col mt-10 items-center h-screen">
+            <h1 className="text-5xl text-purple mb-6">Skills</h1>
+            <ul className="text-gold text-2xl">
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>Javascript</li>
@@ -14,6 +14,13 @@ function Resume() {
                 <li>NoSQL</li>
                 <li>React</li>
             </ul>
+            <a
+                className="mt-14 underline text-purple text-3xl"
+                href={`${process.env.PUBLIC_URL}/${'resume.pdf'}`}
+                download='resume.pdf'
+            >
+                RESUME
+            </a>
         </div>
     )
 }
